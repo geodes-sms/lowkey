fe13f6968bbc4c0f1c9c139f1de70c4a6cb37275
david-istvan <goerenh@gmail.com>
2021-02-11 20:37:26 -0500

    Re-added __eq__, __hash__ and the corresponding test now that the equality is clearer

--- collabserver-modeling/lww/LWWRegister.py
+++ collabserver-modeling/lww/LWWRegister.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import uuid
+
 from lww import LWWRegister
 
 __author__ = "Istvan David"
@@ -14,6 +15,16 @@ Based on Specification 9 in https://hal.inria.fr/file/index/docid/555588/filenam
 
 
 class LWWRegister():
+    
+    def __eq__(self, other):
+        """Overrides the default implementation"""
+        if isinstance(other, LWWRegister):
+            return self.__id == other.getId()
+        return False
+    
+    def __hash__(self):
+        """Overrides the default implementation"""
+        return hash(tuple(sorted(self.__dict__.items())))
     
     def __init__(self, value=None, timestamp:int=0, prototype:LWWRegister=None):
         self.__id = prototype.getId() if prototype else uuid.uuid1()
