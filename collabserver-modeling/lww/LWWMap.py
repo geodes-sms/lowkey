9192668edda072d8d3cd26951dec8bc33d08a621
david-istvan <goerenh@gmail.com>
2021-02-21 11:25:22 -0500

    Removed uuid from the Mapping

--- collabserver-modeling/lww/LWWMap.py
+++ collabserver-modeling/lww/LWWMap.py
@@ -10,29 +10,33 @@ __license__ = "GPL-3.0"
 """
 LWWMap data type.
 Loosely based on the specification https://hal.inria.fr/file/index/docid/555588/filename/techreport.pdf.
+
+Extends the LWWSet type. The keySet is an LWWSet, and a set Mappings is used to link values to the
+keys. For each key, there is one mapping, which holds (i) a reference to the key, (ii) the value, and
+(iii) a timestamp. The construct is augmented with CRDT semantics by the keySet's LWWSet.
 """
 
 
 class Mapping():
 
     def __init__(self, key, value, timestamp):
-        self.__key:LWWRegister = key
-        self.__value:LWWRegister = LWWRegister(value, timestamp)
-        
+        self.__keyRegister:LWWRegister = key
+        self.__valueRegister:LWWRegister = LWWRegister(value, timestamp)
+    
     def getKey(self):
-        return self.__key.query()
+        return self.__keyRegister.query()
     
     def getKeyRegister(self):
-        return self.__key
+        return self.__keyRegister
     
     def getValue(self):
-        return self.__value.query()
+        return self.__valueRegister.query()
     
     def getValueRegister(self):
-        return self.__value
+        return self.__valueRegister
     
     def getTimestamp(self):
-        return self.getValue().getTimestamp()
+        return self.__valueRegister.getTimestamp()
 
 
 class LWWMap():
@@ -51,12 +55,10 @@ class LWWMap():
         return None
     
     def add(self, key, value, timestamp: int):
-        keyRegister = self.__keySet.add(key, timestamp)
+        newKeyRegister = self.__keySet.add(key, timestamp)
         
-        if not keyRegister:
-            pass  # fail silent
-        
-        self.__mappings.add(Mapping(keyRegister, value, timestamp))
+        if newKeyRegister:
+            self.__mappings.add(Mapping(newKeyRegister, value, timestamp))
     
     def remove(self, key, timestamp: int):
         currentMapping = self.__lookupMapping(key)
@@ -67,6 +69,11 @@ class LWWMap():
         self.__mappings.remove(currentMapping)
         self.__keySet.remove(key, timestamp)
     
+    """
+    Update is not supported currently, as it requires further formal analysis regarding its preservation
+    of CTRD consistency properties.
+    """
+    """
     def update(self, key, newValue, timestamp):
         currentMapping = self.__lookupMapping(key)
         
@@ -75,6 +82,7 @@ class LWWMap():
             
         self.__mappings.remove(currentMapping)
         self.__mappings.add(Mapping(self.__keySet.lookup(key), newValue, timestamp))
+    """
                     
     def clear(self, timestamp: int):
         pass
@@ -82,15 +90,27 @@ class LWWMap():
     def size(self):
         return self.__keySet.size()
     
-    """Internal methods"""
-     
-    def __lookupMapping(self, key):
+    """Internal methods""" 
+    
+    def __lookupMappingsForKey(self, key):
+        mappings = set()
+        
+        for m in self.__mappings:
+            if m.getKey() == key:
+                mappings.add(m)
+    
+        return mappings
+    
+    def __latestMappingForKey(self, key):
         mapping = None
         
-        if self.__keySet.query(key):
-            for m in self.__mappings:
-                if m.getKey() == key:
-                    mapping = m
-                    break
+        for m in self.__lookupMappingsForKey(key):
+            if not mapping or mapping.getTimestamp() < m.getTimestamp():
+                mapping = m
+                
+        return mapping
+    
+    def __lookupMapping(self, key):
+        mapping = self.__latestMappingForKey(key)
         
         return mapping
--- collabserver-modeling/lww/tests/LWWMapTests.py
+++ collabserver-modeling/lww/tests/LWWMapTests.py
@@ -11,7 +11,7 @@ __license__ = "GPL-3.0"
 
 class LWWMapTests(unittest.TestCase):
 
-    def testAddEntry(self):
+    def testAddAndQueryEntries(self):
         lwwMap = LWWMap()
         
         key1 = "name"
@@ -28,16 +28,45 @@ class LWWMapTests(unittest.TestCase):
         self.assertEqual(lwwMap.query(key2), value2)
         self.assertEqual(lwwMap.size(), 2)
         
-        value3 = "David"
-        lwwMap.update(key1, value3, 30)
+    def testAddAndQueryDuplicateEntry(self):
+        lwwMap = LWWMap()
         
-        self.assertEqual(lwwMap.query(key1), value3)
-        self.assertEqual(lwwMap.size(), 2)
+        key1 = "name"
+        value1 = "Istvan"
+        lwwMap.add(key1, value1, 10)
+        
+        self.assertEqual(lwwMap.query(key1), value1)
+        self.assertEqual(lwwMap.size(), 1)
+        
+        value2 = "David"
+        lwwMap.add(key1, value2, 20)
+        
+        self.assertEqual(lwwMap.size(), 1)
+        self.assertEqual(lwwMap.query(key1), value2)
+        self.assertNotEqual(lwwMap.query(key1), value1)
+
+        
+    def testRemoveAndQueryEntry(self):
+        lwwMap = LWWMap()
+        
+        key1 = "name"
+        value1 = "Istvan"
+        lwwMap.add(key1, value1, 10)
+        
+        self.assertEqual(lwwMap.query(key1), value1)
+        self.assertEqual(lwwMap.size(), 1)
         
         lwwMap.remove(key1, 30)
-        self.assertEqual(lwwMap.query(key1), None)
-        self.assertEqual(lwwMap.size(), 1)
-        self.assertEqual(lwwMap.query(key2), value2)
+        self.assertEqual(lwwMap.size(), 0)
+
+    """
+    TODO: assert raises KeyError    
+    def testRemoveNotexisting(self):
+        lwwMap = LWWMap()
+        
+        with lwwMap.remove("key", 10):
+            self.assertRaises(KeyError)
+    """    
 
 if __name__ == "__main__":
     unittest.main()
