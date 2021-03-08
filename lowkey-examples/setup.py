ae08fc0eb02391ed07110d1299d2869a078c5962
david-istvan <goerenh@gmail.com>
2021-03-07 20:32:07 -0500

    Turning off the tests for the example for now

--- lowkey-examples/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-import sys
-sys.path.append("..")
-
-setup(
-    name='collabtypes',
-    packages=['collabtypes'],
-    package_dir={'collabtypes': '../lowkey/collabtypes'}
-)
