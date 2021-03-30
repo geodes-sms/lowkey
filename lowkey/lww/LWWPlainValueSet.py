d7cc6dc8067606304270ce3fe9605fb04093857b
david-istvan <goerenh@gmail.com>
2021-03-30 01:42:36 -0400

    Added simplified LWWSet implementation

---
+++ lowkey/lww/LWWPlainValueSet.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+import itertools
+
+_author__ = "Istvan David"
+__copyright__ = "Copyright 2021, GEODES"
+__credits__ = "Eugene Syriani"
+__license__ = "GPL-3.0"
+
+"""
+LWWSet data type, based on the LWW-element-Set specification in
+https://hal.inria.fr/file/index/docid/555588/filename/techreport.pdf.
+
+Provides a redundancy-free collection to store values. Values are stored in tuples with their
+timestamp. The set emulates the behavior of the standard set types: on its public interface, if a
+value exists, only exists once.
+
+The value exists iff:
+-there exists an entry in an addSet corresponding to the value, and
+---there exists no corresponding entry in the removeSet, or
+---there exists a corresponding entry in the removeSet, but with a smaller timestamp.
+
+Otherwise the value does not exist.
+"""
+
+    
+class LWWPlainValueSet():
+            
+    def __init__(self):
+        self.__addSet = set()
+        self.__removeSet = set()
+    
+    def __iter__(self):
+        self.__currentlyExisting = self.__collectExisting()
+        self.__currentIteratorIndex = len(self.__currentlyExisting)
+        return self
+
+    def __next__(self):
+        if self.__currentIteratorIndex > 0:
+            self.__currentIteratorIndex -= 1
+            element = self.__currentlyExisting[self.__currentIteratorIndex]
+            return element
+        else: 
+            raise StopIteration
+    
+    """Interface methods"""
+    
+    def lookup(self, value) -> bool:
+        if not any(value == addedValue[0] for addedValue in self.__addSet):
+            return False
+        if not any(value == removedValue[0] for removedValue in self.__removeSet):
+            return True
+        
+        lastAdded = max(addedValue[1] for addedValue in self.__addSet if value == addedValue[0])
+        lastRemoved = max(removedValue[1] for removedValue in self.__removeSet if value == removedValue[0])
+        
+        return lastAdded > lastRemoved
+        
+    def add(self, newValue, timestamp: int) -> bool:
+        if any(newValue == addedValues[0] and timestamp < addedValues[1] for addedValues in self.__addSet):
+            return False
+        
+        self.__addSet.add((newValue, timestamp))
+        return True
+            
+    def remove(self, value, timestamp: int):
+        self.__removeSet.add((value, timestamp))
+    
+    def clear(self, timestamp: int):
+        if(self.size() == 0):
+            return
+        
+        addedEntries = self.__groupEntriesByValue(self.__addSet)
+        
+        for aKey, _ in addedEntries:
+            if self.lookup(aKey):
+                self.remove(aKey, timestamp)
+    
+    def size(self) -> int:
+        if(len(self.__addSet) == 0):
+            return 0
+        
+        return len(list(self.__collectExisting()))
+    
+    def __groupEntriesByValue(self, entrySet):
+        keyFunc = lambda x: x[0]
+        return itertools.groupby(sorted(entrySet, key=keyFunc), keyFunc)
+    
+    def __collectExisting(self):
+        if len(self.__addSet) == 0:
+            return list()
+        
+        existing = list()
+        for key, group in self.__groupEntriesByValue(self.__addSet):
+            if self.lookup(key):
+                lastAdded = max(addedValue[1] for addedValue in group if key == addedValue[0])
+                existing.append((key, lastAdded))
+                
+        return existing
---
+++ lowkey/lww/tests/LWWPlainValueSetTests.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+import unittest
+
+from lww.LWWPlainValueSet import LWWPlainValueSet
+
+__author__ = "Istvan David"
+__copyright__ = "Copyright 2021, GEODES"
+__credits__ = "Eugene Syriani"
+__license__ = "GPL-3.0"
+
+
+class LWWPlainValueSetTests(unittest.TestCase):
+
+    def testExistsOnEmptySetReturnsFalse(self):
+        lwwSet = LWWPlainValueSet()
+        self.assertFalse(lwwSet.lookup("element"))
+
+    def testAddingElementMakesItExist(self):
+        lwwSet = LWWPlainValueSet()
+        
+        key = "element"
+        self.assertFalse(lwwSet.lookup(key))
+        
+        lwwSet.add(key, 10)
+        self.assertTrue(lwwSet.lookup(key))
+        
+    def testAddingOlderValuesIsOmitted(self):
+        lwwSet = LWWPlainValueSet()
+        
+        key = "element"
+        self.assertEqual(lwwSet.size(), 0)
+        
+        addTime1 = 20
+        addTime2 = 10
+        
+        lwwSet.add(key, addTime1)
+        self.assertEqual(lwwSet.size(), 1)
+        
+        lwwSet.add(key, addTime2)
+        self.assertEqual(lwwSet.size(), 1)
+    
+    def testAddingElementsReflectsInSize(self):
+        lwwSet = LWWPlainValueSet()
+        
+        element1 = "element1"
+        element2 = "element2"
+        
+        self.assertEqual(lwwSet.size(), 0)
+        
+        lwwSet.add(element1, 10)
+        self.assertEqual(lwwSet.size(), 1)
+        
+        lwwSet.add(element2, 20)
+        self.assertEqual(lwwSet.size(), 2)
+        
+    def testRemovingElementsReflectsInSize(self):
+        lwwSet = LWWPlainValueSet()
+        
+        element1 = "element1"
+        element2 = "element2"
+        
+        self.assertEqual(lwwSet.size(), 0)
+        
+        lwwSet.add(element1, 10)
+        self.assertEqual(lwwSet.size(), 1)
+        
+        lwwSet.add(element2, 20)
+        self.assertEqual(lwwSet.size(), 2)
+        
+        lwwSet.remove(element2, 30)
+        self.assertEqual(lwwSet.size(), 1)
+
+    def testAddingDuplicatesIsOmitted(self):
+        lwwSet = LWWPlainValueSet()
+        
+        key1 = "element1"
+        key2 = "element2"
+        self.assertEqual(lwwSet.size(), 0)
+        
+        lwwSet.add(key1, 10)
+        self.assertEqual(lwwSet.size(), 1)
+        
+        lwwSet.add(key2, 20)
+        self.assertEqual(lwwSet.size(), 2)
+        
+        lwwSet.add(key1, 30)
+        self.assertEqual(lwwSet.size(), 2)
+
+    def testRemovedElementDoesNotExist(self):
+        lwwSet = LWWPlainValueSet()
+        
+        key = "element"
+        self.assertFalse(lwwSet.lookup(key))
+        
+        addTime = 10
+        removeTime = 20
+        
+        lwwSet.add(key, addTime)
+        self.assertTrue(lwwSet.lookup(key))
+        
+        lwwSet.remove(key, removeTime)
+        self.assertFalse(lwwSet.lookup(key))
+        
+    def testAddRemoveAdd(self):
+        lwwSet = LWWPlainValueSet()
+        self.assertEqual(lwwSet.size(), 0)
+        
+        element1 = "element1"
+        
+        lwwSet.add(element1, 10)
+        self.assertEqual(lwwSet.size(), 1)
+        
+        lwwSet.remove(element1, 20)
+        self.assertEqual(lwwSet.size(), 0)
+        
+        lwwSet.add(element1, 30)
+        self.assertEqual(lwwSet.size(), 1)
+        
+    def testAddAddRemoveWithDelay(self):
+        lwwSet = LWWPlainValueSet()
+        self.assertEqual(lwwSet.size(), 0)
+        
+        element1 = "element1"
+        
+        lwwSet.add(element1, 10)
+        self.assertEqual(lwwSet.size(), 1)
+        
+        lwwSet.add(element1, 30)
+        self.assertEqual(lwwSet.size(), 1)
+        
+        lwwSet.remove(element1, 20)
+        self.assertEqual(lwwSet.size(), 1)
+    
+    def testSize(self):
+        lwwSet = LWWPlainValueSet()
+        self.assertEqual(lwwSet.size(), 0)
+        
+        element1 = "element1"
+        element2 = "element2"
+        element3 = "element3"
+        
+        lwwSet.add(element1, 10)
+        self.assertEqual(lwwSet.size(), 1)
+        
+        lwwSet.add(element1, 15)
+        self.assertEqual(lwwSet.size(), 1)
+        
+        lwwSet.add(element2, 20)
+        self.assertEqual(lwwSet.size(), 2)
+        
+        lwwSet.remove(element1, 25)
+        self.assertFalse(lwwSet.lookup(element1))
+        self.assertEqual(lwwSet.size(), 1)
+        
+        lwwSet.remove(element1, 30)
+        self.assertEqual(lwwSet.size(), 1)
+        
+        lwwSet.remove(element3, 35)
+        self.assertEqual(lwwSet.size(), 1)
+        
+        lwwSet.add(element3, 40)
+        self.assertEqual(lwwSet.size(), 2)
+        
+        lwwSet.remove(element2, 45)
+        self.assertEqual(lwwSet.size(), 1)
+        
+        lwwSet.remove(element3, 50)
+        self.assertEqual(lwwSet.size(), 0)
+    
+    def testClear(self):
+        lwwSet = LWWPlainValueSet()
+        self.assertEqual(lwwSet.size(), 0)
+        
+        lwwSet.add("element1", 10)
+        self.assertEqual(lwwSet.size(), 1)
+        
+        lwwSet.add("element2", 20)
+        self.assertEqual(lwwSet.size(), 2)
+        
+        lwwSet.clear(30)
+        self.assertEqual(lwwSet.size(), 0)
+
+    def testIterations(self):
+        lwwSet = LWWPlainValueSet()
+        
+        lwwSet.add("element1", 10)    
+        lwwSet.add("element2", 20)
+        self.assertEqual(lwwSet.size(), 2)
+        
+        i = 0
+        for _ in lwwSet:
+            i += 1
+        self.assertEqual(i, 2)
+        
+        lwwSet.add("element3", 30)
+        self.assertEqual(lwwSet.size(), 3)
+        
+        i = 0
+        for _ in lwwSet:
+            i += 1
+        self.assertEqual(i, 3)
+        
+        lwwSet.remove("element2", 40)
+        self.assertEqual(lwwSet.size(), 2)
+        
+        i = 0
+        for _ in lwwSet:
+            i += 1
+        self.assertEqual(i, 2)
+        
+        lwwSet.add("element3", 50)
+        self.assertEqual(lwwSet.size(), 2)
+        
+        i = 0
+        for _ in lwwSet:
+            i += 1
+        self.assertEqual(i, 2)
+        
+        lwwSet.clear(60)
+        self.assertEqual(lwwSet.size(), 0)
+        
+        i = 0
+        for _ in lwwSet:
+            i += 1
+        self.assertEqual(i, 0)  
+    
+    def testIterationContents(self):
+        lwwSet = LWWPlainValueSet()
+        
+        elements = ["element1", "element2"]
+        
+        lwwSet.add(elements[0], 10)    
+        lwwSet.add(elements[1], 20)
+        self.assertEqual(lwwSet.size(), 2)
+        
+        for value, _ in lwwSet:
+            self.assertTrue(value in elements)
+
+
+if __name__ == "__main__":
+    unittest.main()
