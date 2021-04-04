be8c943f886204f2ae779f748672bfb39025dbae
david-istvan <goerenh@gmail.com>
2021-04-03 21:34:21 -0400

    Maybe now

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'lowkey',
+    description = 'Package for building&testing purposes only.',
+    packages = ['lowkey', 'lowkey.collabtypes', 'lowkey.lww', 'lowkey.server'], 
+    version = '0.0.1',
+    url = 'https://github.com/david-istvan/lowkey',
+    author = 'Istvan David',
+    author_email = 'istvan@lowkey-github.io'
+) 
