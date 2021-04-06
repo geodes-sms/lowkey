a330728ecce4c69c420ee118da98011de8258dd2
david-istvan <goerenh@gmail.com>
2021-04-06 19:24:08 -0400

    Update EntityTests.py

--- lowkey/collabtypes/tests/EntityTests.py
+++ lowkey/collabtypes/tests/EntityTests.py
@@ -18,41 +18,54 @@ def halt():
 class EntityTests(unittest.TestCase):
 
     def testEntityCreation(self):
-        entity = Entity()
+        person = Entity()
         
         attributeName1 = "name"
         attributeValue1 = "Istvan"
-        entity._setAttribute(attributeName1, attributeValue1)
+        person._setAttribute(attributeName1, attributeValue1)
         halt()
         
         attributeName2 = "profession"
         attributeValue2 = "Researcher"
-        entity._setAttribute(attributeName2, attributeValue2)
+        person._setAttribute(attributeName2, attributeValue2)
         halt()
         
-        returnValue1 = entity._getAttribute(attributeName1)
-        returnValue2 = entity._getAttribute(attributeName2)
+        returnValue1 = person._getAttribute(attributeName1)
+        returnValue2 = person._getAttribute(attributeName2)
         
         self.assertEqual(returnValue1, attributeValue1)
         self.assertEqual(returnValue2, attributeValue2)
         
+        attributeName3 = "name"
+        attributeValue3 = "University of Montreal"
+        university = Entity()
+        university._setAttribute(attributeName3, attributeValue3)
+        halt()
+        
+        returnValue3 = university._getAttribute(attributeName3)
+        self.assertEqual(returnValue3, attributeValue3)
+        
         relationship = Relationship()
+        relationship.setFrom(person)
+        halt()
+        relationship.setTo(university)
+        halt()
+        relationship.setAggregation(False)
+        halt()
         relationship._setAttribute("name", "affiliation")
         halt()
         relationship._setAttribute("directed", "target")
         halt()
-        
-        entity2 = Entity()
-        entity2._setAttribute("name", "University of Montreal")
+        person._addRelationship(relationship)
         halt()
         
-        entity._addRelationship(relationship)
-        halt()
-        
-        affiliationRelationship = entity._getRelationship("affiliation")
+        affiliationRelationship = person._getRelationship("affiliation")
         affiliationRelationship = affiliationRelationship[0] if len(affiliationRelationship) == 1 else self.fail()
-        direction = affiliationRelationship._getAttribute("directed")
         
+        self.assertEqual(affiliationRelationship.getFrom(), person)
+        self.assertEqual(affiliationRelationship.getTo(), university)
+        self.assertFalse(affiliationRelationship.isAggregation())
+        direction = affiliationRelationship._getAttribute("directed")
         self.assertEqual(direction, "target")
 
 
