aa3c75cdf86b1ca61d5a880be170a63584361cda
david-istvan <goerenh@gmail.com>
2021-04-12 23:48:43 -0400

    Removed unused parts from the Mindmap example

--- lowkey-examples/mindmap/metamodel/relationships/CentralTopic_MainTopic_mainTopics.py
+++
@@ -1,7 +0,0 @@
-from collabtypes.Relationship import Relationship
-
-class CentralTopic_MainTopic_mainTopics(Relationship):
-    
-    def __init__(self, name):
-        super().__init__('mainTopics')
-        
