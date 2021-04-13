0cb820aef3d3a17b956e31dc46f2c4bb99d00ca3
david-istvan <goerenh@gmail.com>
2021-04-12 23:39:34 -0400

    Fixed tests based on the example

--- lowkey-examples/mindmap/tests/BuildMindmapTest.py
+++ lowkey-examples/mindmap/tests/BuildMindmapTest.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
-import time
 import unittest
 
-from mindmap import MindmapPrinter
-from mindmap.metamodel.entities.CentralTopic import CentralTopic
-from mindmap.metamodel.entities.MainTopic import MainTopic
-from mindmap.metamodel.entities.Marker import Marker
+from collabtypes.Clock import Clock, ClockMode
 from mindmap.metamodel.entities.MindMap import MindMap
-from mindmap.metamodel.entities.SubTopic import SubTopic
 
 __author__ = "Istvan David"
 __copyright__ = "Copyright 2021, GEODES"
@@ -15,65 +10,18 @@ __credits__ = "Eugene Syriani"
 __license__ = "GPL-3.0"
 
 
-def halt():
-    time.sleep(0.001)
-
-
 class BuildMindmapTest(unittest.TestCase):
 
-    def testBuildMindmap(self): 
-        # Create MindMap
-        title = 'improveTeachingRecord'
-        mindmap = MindMap(title)
-        halt()
+    def testRenameMindmap(self):
+        Clock.setUp(ClockMode.DEBUG)
         
-        self.assertEqual(mindmap.getTitle(), title)
+        title1 = 'improveTeachingRecord'
+        mindmap = MindMap(title1)
+        self.assertEqual(mindmap.getTitle(), title1)
         
-        newTitle = 'improvePublicationRecord'
-        mindmap.setTitle(newTitle)
-        halt()
-        self.assertEqual(mindmap.getTitle(), newTitle)
-        
-        # Create CentralTopic and add to the MindMap
-        centralTopicName = 'publishPaper'
-        centralTopic = CentralTopic(centralTopicName)
-        mindmap.setTopic(centralTopic)
-        halt()
-        self.assertEqual(centralTopic.getName(), centralTopicName)
-        
-        self.assertEqual(mindmap.getTopic(), centralTopic)
-        self.assertEqual(mindmap.getTopic().getName(), centralTopicName)
-        
-        """
-        # Create two MainTopics and add them to the CentralTopic
-        mt1 = MainTopic('experiment')
-        c.addMainTopic(mt1)
-        halt()
-        
-        # #Create this one with a missing argument
-        mt2 = MainTopic()
-        mt2.setName('writePaper')
-        halt()
-        c.addMainTopic(mt2)
-        halt()
-        
-        # Create two SubTopics and add them to one of the MainTopics
-        s1 = SubTopic('relatedWork')
-        mt2.addSubTopic(s1)
-        halt()
-        s2 = SubTopic('contributions')
-        mt2.addSubTopic(s2)
-        halt()
-        
-        # Create a Marker
-        x = Marker('x')
-        mindmap.addMarker(x)
-        halt()
-        s2.setMarker(x)
-        halt()
-        """
-        # Print the MindMap
-        # MindmapPrinter.printMindmap(mindmap)
+        title2 = 'improvePublicationRecord'
+        mindmap.setTitle(title2)
+        self.assertEqual(mindmap.getTitle(), title2)
 
         
 if __name__ == "__main__":
