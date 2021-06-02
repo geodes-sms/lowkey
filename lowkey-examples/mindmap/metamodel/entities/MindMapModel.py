1e9a2f244accf826ce8f90693db9707ce8aaaa40
david-istvan <goerenh@gmail.com>
2021-06-01 21:05:50 -0400

    Added model abstraction to the MindMap example

---
+++ lowkey-examples/mindmap/metamodel/entities/MindMapModel.py
@@ -0,0 +1,20 @@
+from collabtypes.Model import Model
+
+
+class MindMapModel(Model):
+    
+    def __init__(self, title=""):
+        super().__init__()
+        self.setTitle(title)
+
+    # title: Attribute
+    # ========================
+    # Multiplicity: 1
+    # Type: String
+    # ========================
+    # Methods: get, set
+    def getTitle(self):
+        return self.getAttribute("title")
+
+    def setTitle(self, title):
+        self.setAttribute("title", title)
